84bf1738234724f695ac2fafeafa27c890088d09
Prakhar Kumar Srivastava <prakhar25789@gmail.com>
2023-08-20 01:48:43 +0530

    FIX: Cleaning up all the emails after form submission

--- src/Components/DateTimePicker/TimePicker.js
+++ src/Components/DateTimePicker/TimePicker.js
@@ -5,6 +5,7 @@ import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { TimePicker } from '@mui/x-date-pickers/TimePicker';
 
 export default function BasicTimePicker({time,setTime}) {
+  
   return (
       <DemoContainer components={['TimePicker']}>
         <TimePicker label="Enter the time of Interview" value={time} onChange={(newTime)=> setTime(newTime)} />
